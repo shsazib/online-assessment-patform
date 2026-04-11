fd6d160b88ed255c0f769786f53af8b8c417b7d4
shsazib <shsazibbhuiyan892939@gmail.com>
2026-04-11 23:31:28 +0600

    loading page remove

--- src/app/(auth)/loading.tsx
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center min-h-screen">
-      <div className="text-lg font-semibold animate-pulse">
-        Loading...
-      </div>
-    </div>
-  );
-}
